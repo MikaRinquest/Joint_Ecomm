508e1be1f1a1299f21b4f72eb4bf0c9d8dca943b
Mika Rinquest <rinquestmika@gmail.com>
2022-08-15 11:36:51 +0200

    Added cart functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const con = require("../library/database_connection");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const middelware = require("../middleware/auth");
+// const middleware = require("../middleware/auth");
 
 // Display all users
 router.get("/", (req, res) => {
@@ -37,16 +37,14 @@ router.get("/:id", (req, res) => {
 router.post("/register", (req, res) => {
   try {
     let sql = "INSERT INTO users SET ?";
-    const { fullname, type, email, password, shipping_address, phone_number } =
+    const { fullname, email, password, shipping_address, phone_number } =
       req.body;
-
     // Start encrypting
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
 
     let user = {
       fullname,
-      type,
       email,
       password: hash,
       shipping_address,
@@ -145,7 +143,6 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   try {
     let sql = `DELETE FROM users WHERE user_id = ${req.params.id}`;
-
     con.query(sql, (err, result) => {
       if (err) throw err;
       res.send("User has been successfully deleted");
@@ -169,4 +166,105 @@ router.get("/users/verify", (req, res) => {
   });
 });
 
+// Get all items from cart
+router.get("/:id/cart", (req, res) => {
+  let cart = [];
+  try {
+    let sql = "SELECT * FROM cart";
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.send("Cart is empty.");
+      } else {
+        res.send(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// Adding to cart
+router.post("/:id/cart", (req, res) => {
+  try {
+    let sql = "INSERT INTO cart SET ?";
+    const { user_id, quantity, cart_items } = req.body;
+    let jsonCart = JSON.stringify(cart_items);
+    let cart = {
+      user_id,
+      quantity,
+      cart_items: jsonCart,
+    };
+    con.query(sql, cart, (err, result) => {
+      if (err) throw err;
+      res.send("Added to cart successfully.");
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// Deleting one item from cart
+router.delete("/:id/cart/:id", (req, res) => {
+  try {
+    let sql = `DELETE FROM cart WHERE cart_id = ${req.params.id}`;
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send("Item has been successfully removed.");
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// Clear cart
+router.delete("/:id/cart", (req, res) => {
+  try {
+    let sql = "TRUNCATE TABLE cart";
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send("Cart has been cleared");
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// Edit cart
+router.patch("/:id/cart/:id", (req, res) => {
+  try {
+    let sql = "UPDATE cart SET ?";
+    let cart = {
+      quantity: req.body.quantity,
+    };
+    con.query(sql, cart, (err, result) => {
+      if (err) throw err;
+      res.send("Quantity has been updated");
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+/*
+{
+"user_id":"1",
+"quantity":"2",
+"cart_items":[{"product_id": 3,
+    "name": "Love Bracelet",
+    "brand": "Cartier",
+    "description": "Beautiful bracelet for your love.",
+    "price": 10000,
+    "type": "women's jewelry",
+    "state": "new",
+    "url": "https://i.postimg.cc/B6LJ6mN5/pngwing-com-7.png",
+    "user_id": 1,
+    "qty":2}]
+}
+*/
 module.exports = router;
